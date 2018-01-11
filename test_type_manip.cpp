4b7d291fb727cf7247031f9d18d66e8c940648d9
wangoasis <wangqingsky@outlook.com>
2018-01-11 22:32:48 +0800

    change cout to assert in test_type_manip.cpp

--- test_type_manip.cpp
+++ test_type_manip.cpp
@@ -1,5 +1,5 @@
 #include "type_manip.h"
-#include <iostream>
+#include <cassert>
 
 class Base {
 public:
@@ -14,17 +14,15 @@ public:
 int main() {
     Base b;
     Derived d;
-    std::cout << SUPER_SUBCLASS(Base, Derived) << std::endl; // true
-    std::cout << SUPER_SUBCLASS(Derived, Base) << std::endl; // false
-    std::cout << SUPER_SUBCLASS(Derived, Derived) << std::endl;// true
-    std::cout << SUPER_SUBCLASS(Derived, void) << std::endl; // false
-    std::cout << std::endl;
-    std::cout << SUPER_SUBCLASS_STRICT(Base, Derived) << std::endl;// true
-    std::cout << SUPER_SUBCLASS_STRICT(Derived, Base) << std::endl; // false
-    std::cout << SUPER_SUBCLASS_STRICT(Derived, Derived) << std::endl; // false
-    std::cout << SUPER_SUBCLASS_STRICT(Base, void) << std::endl; // false
-    std::cout << std::endl;
-    std::cout << IS_SAME_TYPE(Base, Base) << std::endl;// true
-    std::cout << IS_SAME_TYPE(Derived, Derived) << std::endl;// true
-    std::cout << IS_SAME_TYPE(Base, Derived) << std::endl; // false
+    assert(SUPER_SUBCLASS(Base, Derived) == 1); // true
+    assert(SUPER_SUBCLASS(Derived, Base) == 0); // false
+    assert(SUPER_SUBCLASS(Derived, Derived) == 1);// true
+    assert(SUPER_SUBCLASS(Derived, void) == 0);  // false
+    assert(SUPER_SUBCLASS_STRICT(Base, Derived) == 1);// true
+    assert(SUPER_SUBCLASS_STRICT(Derived, Base) == 0); // false
+    assert(SUPER_SUBCLASS_STRICT(Derived, Derived) == 0); // false
+    assert(SUPER_SUBCLASS_STRICT(Base, void) == 0); // false
+    assert(IS_SAME_TYPE(Base, Base) == 1); // true
+    assert(IS_SAME_TYPE(Derived, Derived) == 1);// true
+    assert(IS_SAME_TYPE(Base, Derived) == 0); // false
 }
